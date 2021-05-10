eddb07e25e436283df7dd22fc7f12223f3a7eeb6
aw-test-93 <81768906+aw-test-93@users.noreply.github.com>
2021-05-10 12:59:14 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+  
+echo WORKING DIR
+pwd
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo SCRIPT PATH
+echo $SCRIPTPATH
+
+sudo unlink ~/.bashrc
+ln -sv $SCRIPTPATH/.bashrc ~
+
+source ~/.bashrc
